afcefd9be7bd9d10d2edd1623c5c127eaf5cdcbb
surya ramadan <suryacbs123@gmail.com>
2024-01-18 09:34:04 +0700

    merubah fileupload

--- controllers/productrouter.js
+++ controllers/productrouter.js
@@ -67,7 +67,7 @@ export const postproduct = async (req,res)=>{
     const filesize = file.data.length
     const ext = path.extname(file.name)
     const fileName = file.md5 + ext;
-    const url = `${req.protocol}://"https://apistore.cyclic.app"/images/${fileName}`
+    const url = `${req.protocol}://https://apistore.cyclic.app/images/${fileName}`
     const allowType = ['.png','.jpg','.jpeg']
 
     if(!namaproduct || !category || !hargaawal 
